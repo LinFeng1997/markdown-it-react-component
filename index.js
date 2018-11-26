d7be39127d259fca8e913f2fb7cc29960cde8870
LinFeng1997 <ynublow@gmail.com>
2018-11-26 23:21:19 +0800

    refactor: marker should be ` or :

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 import container from 'markdown-it-container'
-import replacer from './replacer'
+import replacer from './lib/replacer'
 
 export const SupportReactComponent = (md, options) => {
   md.use(...createContainer('rc', options))
+  md.use(...createContainer('rc', options, '`'))
 }
 
-function createContainer(klass, options) {
+function createContainer (klass, options, marker = ':') {
   return [container, klass, {
-    render(tokens, idx) {
+    render (tokens, idx) {
       const token = tokens[idx]
       const info = token.info.trim().slice(klass.length).trim()
       if (token.nesting === 1) {
-        let wrapperId = 'rc' + Math.random().toString(36).substr(2, 10);
+        let wrapperId = 'rc' + Math.random().toString(36).substr(2, 10)
         setTimeout(() => {
           replaceReactCode(wrapperId, options)
-        }, 0);
+        }, 0)
         return `<div class="${klass} minxin-react" style="opacity: 0" id="${wrapperId}">${info}`
       } else {
         return `</div>\n`
       }
-    }
+    },
+    marker
   }]
 }
 
-function replaceReactCode(wrapperId, options = {}) {
+function replaceReactCode (wrapperId, options = {}) {
   const container = document.querySelector(`#${wrapperId} p`)
   const str = container.innerText
   replacer({
