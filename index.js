5953d374ae01cacce2f69dae03b0c08d6af10d26
许云峰 <xuyunfeng.fe@bytedance.com>
2019-03-01 16:29:59 +0800

    chore:error handle

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ function createContainer (klass, options, marker = ':') {
       if (token.nesting === 1) {
         let wrapperId = 'rc' + Math.random().toString(36).substr(2, 10);
 
-        const offset = tokens.slice(idx).findIndex(token => token.type === `container_${klass}_close`);
-        let str = tokens.slice(idx,idx + offset).reduce((pre,cur)=>{
-          return pre + '\n' + cur.content;
-        },'');
-        const html = replacer.getHtml(wrapperId, str);
-        return `<div class="${klass}" style="opacity: 0" id="${wrapperId}">${html}`;
+        try {
+          const offset = tokens.slice(idx).findIndex(token => token.type === `container_${klass}_close`);
+          let str = tokens.slice(idx,idx + offset).reduce((pre,cur)=>{
+            return pre + '\n' + cur.content;
+          },'');
+          const html = replacer.getHtml(wrapperId, str);
+          return `<div class="${klass}" style="opacity: 0" id="${wrapperId}">${html}`;
+        } catch (e) {
+          return `<div class="${klass}" style="opacity: 0" id="${wrapperId}">`;
+        }
       } else {
         return '</div>\n';
       }
